a8d64cd89ab03947254cd13622fc8b77adf97d53
Manuu624 <crackseba201@gmail.com>
2023-06-16 11:13:32 -0300

    creamos un componente pages e importamos el modulo a app.module

--- src/app/modules/auth/pages/login/login.component.spec.ts
+++ src/app/modules/auth/pages/login/login.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ManuuComponent } from './manuu.component';
+import { LoginComponent } from './login.component';
 
-describe('ManuuComponent', () => {
-  let component: ManuuComponent;
-  let fixture: ComponentFixture<ManuuComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ManuuComponent]
+      declarations: [LoginComponent]
     });
-    fixture = TestBed.createComponent(ManuuComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
